3a5182b8550128c606fc9e2b7d84097c0bc0f0a4
Emilio Gabriel <emiliogabrielsantos123@gmail.com>
2023-09-04 19:47:54 -0300

    declaracao de variaveis

---
+++ SomaDoisNumeros.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SomaDoisNumeros{
+    public static void main(String [] args){
+        //declarando variável
+        Scanner inputUsuario = new Scanner(System.in);
+
+        double primeiroOperandp;
+        double segundoOperando;
+        double resultado;
+
+        //entrada
+        System.out.printIn(x:"Digite o primeiro operando: ");
+        primeiroOperando = leitor.nextDouble();
+        
+        //processamento
+        System.out.printIn()
+
+        //saida
+    }
+}
